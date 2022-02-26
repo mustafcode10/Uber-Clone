e70829048f9d6fc87ef8f120525717ce312b4c5f
Mustaf <mustafdiiriye@hotmail.com>
2022-02-26 16:43:37 +0200

    design

--- App.js
+++ App.js
@@ -1,18 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
-import  {store} from "./store";
-import HomeScreen from './screens/HomeScreen';
+import { store } from "./store";
+import HomeScreen from "./screens/HomeScreen";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
-  console.log("store", store)
+  console.log("store", store);
   return (
-    <Provider store={store} >
-      {/* <View style={styles.container}>
-        <Text>Uber </Text>
-      </View> */}
-      <HomeScreen />
-     </Provider>
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <HomeScreen />
+      </SafeAreaProvider>
+    </Provider>
   );
 }
 
--- components/NavOptions.js
+++ components/NavOptions.js
@@ -7,6 +7,7 @@ import {
   Image
 } from "react-native";
 import React from "react";
+import {Icon} from "react-native-elements"
 
 const data = [
   {
@@ -31,7 +32,7 @@ const NavOptions = () => {
       keyExtractor={(item) => item.id}
       renderItem={({ item }) => (
         <TouchableOpacity style={styles.navOptionsStyle} >
-          <View>
+          <View >
            <Image
            style={{ width: 120, height: 120, resizeMode: 'contain' }}
             source={{
@@ -39,7 +40,9 @@ const NavOptions = () => {
             }}
            />
            <Text style={{marginTop: 2, fontWeight: "bold"}}>{item.title}</Text>
+           <Icon style={styles.iconStyle} name="arrowright" color="white" type="antdesign" />
           </View>
+          
         </TouchableOpacity>
       )}
     />
@@ -55,8 +58,21 @@ const styles = StyleSheet.create({
         paddingBottom: 8,
         paddingTop: 4,
         backgroundColor: "#d3d3d3",
-        margin: 2,
-        width: 175,
-        height: 175
+        margin: 10,
+        width: 170,
+        height: 215,
+        justifyContent: "center",
+        alignItems: "center",
+        alignSelf: "center",
+    },
+    iconStyle: {
+        width: 35,
+        height: 35,
+        backgroundColor: "black",
+        borderRadius: 35/2,
+        padding: 2,
+        alignItems: "center",
+        justifyContent: "center",
+
     }
 });
